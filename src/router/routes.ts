aee4f0abc9a223a57a439b6f1a81c088254ac229
zhaolong <naokimidori@163.com>
2021-03-18 22:32:13 +0800

    feat: route

---
+++ src/router/routes.ts
@@ -0,0 +1,15 @@
+import {
+  Index,
+  About
+} from '../pages/index';
+
+export const routes = [
+  {
+    path: '/',
+    component: Index,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+];

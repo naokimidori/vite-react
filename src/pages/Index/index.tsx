3cbc13ba454e43559d201f65cac54334c2757f5b
zhaolong <naokimidori@163.com>
2021-08-23 21:40:53 +0800

    feat: useReducer

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { Button } from 'antd'
 import { GET } from '../../utils/request'
 import './index.less'
 
-function Index() {
+interface IState {
+  count: number
+}
+
+interface IAction {
+  type: string
+  payload: number | null
+}
+
+const initCounter: IState = { count: 100 };
+
+const reducer = (state: IState, action: IAction) => {
+  const { type, payload } = action;
+  const { count } = state;
+  switch (type) {
+    case "increment":
+      return { count: payload ? payload + count : count + 1 };
+    case "decrease":
+      return { count: payload ? count - payload : count  - 1 };
+    default:
+      return { count: 0 };
+  }
+};
+
+const Index: React.FC = () => {
   console.log('import.meta.env', import.meta.env)
+
+  const [state, dispatch] = useReducer(reducer, initCounter)
+
   useEffect(() => {
-    GET('/index-infos').then(() => {
-      
-    })
+    GET('/index-infos').then((res) => {})
   }, [])
+
   return (
     <div className="page-index">
-      <Button type='primary'>btn</Button>
+      <h1>{state.count}</h1>
+      <Button type='primary' onClick={() => dispatch({type: 'decrease', payload: 10 })}>btn</Button>
     </div>
   )
 }
 
-export default Index;
+export default Index

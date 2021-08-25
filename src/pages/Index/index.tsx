481109277a9728b724720f29fb79164c95b58edd
zhaolong <naokimidori@163.com>
2021-08-25 23:47:31 +0800

    feat: formik errors

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -9,25 +9,45 @@ function Index() {
       username: '',
       password: ''
     },
-    onSubmit: values => {  
+    validate: values => {
+      const errors: any = {}
+      if (!values.username) {
+        errors.username = '请输入用户名'
+      } else if (values.username.length > 15) {
+        errors.username = '用户名长度不能大于15'
+      }
+      if (!values.password) {
+        errors.password = '请输入密码'
+      } else if (values.password.length < 6) {
+        errors.password = '密码不能小于6位'
+      }
+      return errors
+    },
+    onSubmit: values => {
       console.log(values)
     }
   })
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      姓名：<input
-        type='text'
-        name='username'
-        value={formik.values.username}
-        onChange={formik.handleChange}
-      />
-      密码：<input
-        type='password'
-        name='password'
-        value={formik.values.password}
-        onChange={formik.handleChange}
-      />
+      <div>
+        用户名：<input
+          type='text'
+          name='username'
+          value={formik.values.username}
+          onChange={formik.handleChange}
+        />
+       <span className="tips">{formik.errors.username}</span>
+      </div>
+      <div>
+        密码：<input
+          type='password'
+          name='password'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <span className="tips">{formik.errors.password}</span>
+      </div>
       <input type='submit' />
     </form>
   )
